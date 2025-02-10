41fc3c7dd4b079d5f1c704c1d3d8d6fa4fe55cc3
IvanKazangi12345 <131653342+IvanKazangi12345@users.noreply.github.com>
2025-02-10 13:26:31 +0200

    1

--- 09.02.25_hw_py.py
+++ 09.02.25_hw_py.py
@@ -1,18 +1,14 @@
 start = int(input("Введите первое число: "))
 end = int(input("Введите второе число: "))
-
 if start > end:
     start, end = end, start
-
 print("Нечетные числа в указанном диапазоне:")
 i = start
 while i <= end:
     if i % 2 != 0:
         print(i, end=" ")
     i += 1
-
-print("\n")
-
+    
 count = 0
 num = 100
 while num <= 9999:
@@ -25,5 +21,4 @@ while num <= 9999:
     if unique:
         count += 1
     num += 1
-
 print(f"Количество чисел с разными цифрами в диапазоне 100-9999: {count}")
